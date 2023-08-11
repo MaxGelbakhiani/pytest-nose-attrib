661a39edad82d9666c91f310b300e178680cdda5
Maksim Gelbakhiani <max@nspcc.ru>
2023-08-11 14:21:26 +0400

    added setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 import pytest_nose_attrib
 
 setup(
-    name='pytest-nose-attrib_plugin',
+    name='pytest-nose-attrib',
     description='pytest plugin to use nose @attrib marks decorators'
                 'and pick tests based on attributes and'
                 'partially uses nose-attrib plugin approach',
@@ -11,7 +11,7 @@ setup(
     author_email='Max.Gelbakhiani@gmail.com',
     packages=['pytest_nose_attrib'],
     entry_points={'pytest11': ['attr = pytest_nose_attrib.plugin']},
-    install_requires=['pytest>=7.2'],
+    install_requires=['pytest>=2.2'],
     license="MIT License",
     package_data={'pytest_attrib': ["VERSION"]},
 )
